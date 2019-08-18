2151bfdb71364088c9850cd376145e0940c29880
Fabrício Borgatto <fabricio.borgatto@gmail.com>
2019-08-18 13:08:35 -0300

    atualização

--- main.js
+++ main.js
@@ -8,25 +8,100 @@ let mongoUrl = "mongodb+srv://"+config.mongoUser+":"+config.mongoPass+"@"+config
 MongoClient.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, db) {
 	if(err) throw err;
 	var dbo = db.db("tweeterData")
-	dbo.collection("lastTweets").find().sort({user_followers_count:-1}).toArray(function(err, result) {
-		if(err) throw err;
-		for(var tweet of result) {
-			dbo.collection("mostFollowedUsers").updateOne({user_name: tweet.user_name}, 
-				{ $setOnInsert: 
-					{
-						user_id: tweet.user_id, 
-						user_name: tweet.user_name,
-						user_screen_name: tweet.user_screen_name,
-						user_followers_count: tweet.user_followers_count,
-						hashtag: tweet.hashtag
-					}
-				}, {upsert: true}, function(err, data) {
-				if(err)
-					console.log(err)
-			})
+	
+	dbo.collection("lastTweets").aggregate([
+	{
+		'$group': { 
+			'_id': { 
+				user_id: '$user_id',
+				user_name: '$user_name',
+				user_screen_name: '$user_screen_name',
+				user_followers_count: '$user_followers_count'
+			}
 		}
-		db.close()
-	})
+	},
+	{
+		'$sort': {
+			'_id.user_followers_count': -1
+		}
+	},
+	{
+		'$project': {
+			'_id': 0,
+			'user_id': '$_id.user_id',
+			'user_name': '$_id.user_name',
+			'user_screen_name': '$_id.user_screen_name',
+			'user_followers_count': '$_id.user_followers_count'
+		}
+	}]).toArray(function(err, results){
+		if(err) throw err
+			console.log(results)
+		dbo.collection("mostFollowedUsers").insertMany(results, function(err, result) {
+			if(err) throw err
+		})
+
+	});
+
+	
+	dbo.collection("lastTweets").aggregate([
+	{
+		'$group': {
+			'_id': { $hour: '$created_at' },
+			count: { $sum: 1 }
+		}
+	},
+	{
+		'$sort': {
+			'_id': 1
+		}
+	},
+	{
+		'$project': {
+			'_id': 0,
+			'dayHour': '$_id',
+			'tweets': '$count'
+		}
+	}]).toArray(function(err, results){
+		if(err) throw err
+			console.log(results)
+		dbo.collection("tweetsPerHour").insertMany(results, function(err, result) {
+			if(err) throw err
+
+		})
+
+	});
+	
+	dbo.collection("lastTweets").aggregate([
+	{
+		'$group': {
+			'_id': { hashtag: '$hashtag', lang: '$lang', location: '$user_location' },
+			count: { $sum: 1 },
+		}
+	},
+	{
+		'$sort': {
+			'_id.hashtag': 1,
+			'count': -1
+		}
+	},
+	{
+		'$project': {
+			'_id': 0,
+			'hashtag': '$_id.hashtag',
+			'tweets': '$count',
+			'lang': '$_id.lang',
+			'user_location': '$_id.location'
+		}
+	}]).toArray(function(err, results){
+		if(err) throw err
+			console.log(results)
+		dbo.collection("tweetsPerLangAndLocation").insertMany(results, function(err, result) {
+			if(err) throw err
+
+		})
+
+	});
+	db.close()
 })
 
 
